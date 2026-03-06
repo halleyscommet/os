0ee40376e1faa799a441a0a2890dda4f5dda3c2c
Haley <nharp335@gmail.com>
2026-03-06 10:27:04 -0500

    add some more comments to boot.asm so i can remember

--- boot.asm
+++ boot.asm
@@ -8,12 +8,12 @@ start:
     mov ss, ax ; clear ss
     mov sp, 0x7C00 ; stack starts here and grows downwards
 
-    mov si, msg
-    call print
+    mov si, msg ; moves the message bytes over to something lodsb can use
+    call print ; call the print "function"
 
-%include "print.asm"
+%include "print.asm" ; basically just an import
 
-msg db 'Loading kernel...', 0
+msg db 'Loading kernel...', 0 ; the message, plus the null terminator
 
 times 510 - ($ - $$) db 0 ; pad the boot sector with zeroes
-dw 0xAA55
+dw 0xAA55 ; magic number !! its really 0x55 0xAA, but endianness messes with it?
