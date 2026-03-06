ba87844ebe605ad68bb0f06927ac2059fb91939b
Haley <nharp335@gmail.com>
2026-03-06 11:12:58 -0500

    bro idk what i just did

--- learning/clear_screen.asm
+++
@@ -1,5 +0,0 @@
-clear_screen:
-    mov ah, 0x00 ; puts bios into text mode (even if it already is)
-    mov al, 0x03 ; end of text
-    int 0x10 ; prints
-    ret ; screen is now cleared, we can return
--- run.sh
+++
@@ -1,9 +0,0 @@
-nasm -f bin boot.asm -o boot.bin
-
-nasm -f elf32 kernel_entry.asm -o kernel_entry.o
-i686-elf-gcc -m32 -ffreestanding -fno-builtin -nostdlib -c kernel.c -o kernel.o
-i686-elf-ld -T linker.ld kernel_entry.o kernel.o -o kernel.elf
-i686-elf-objcopy -O binary kernel.elf kernel.bin
-
-cat boot.bin kernel.bin > os.img
-qemu-system-x86_64 -drive format=raw,file=os.img
